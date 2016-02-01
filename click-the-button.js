d37e44a34abf051fb8e22e8394de87210e9324be
Thomas Wright <tom.tdw@gmail.com>
2016-02-01 15:06:13 +0000

    Commit the initial files, and the code for the click-the-button exercises

---
+++ click-the-button.js
@@ -0,0 +1,6 @@
+/* This is our first Javascript file. It is currently empty but we will expand
+this to add interactive functionality to the page. */
+
+/* Notice that comments look a bit different in Javascript than in HTML. */
+
+// You can also put comments at the end of a line, like this
